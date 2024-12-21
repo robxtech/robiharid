282054458c258ab73a51e1c7bf832f19e70a777f
berkekayada <berkekayada34@gmail.com>
2024-12-21 13:31:09 +0300

    fix

--- components/header.vue
+++ components/header.vue
@@ -11,7 +11,9 @@ const scrollToContact = () => {
 <template>
   <div class="w-full flex justify-center items-center px-8 lg:px-[120px]">
     <div class="w-full flex flex-row justify-between items-center py-6 gap-4 max-w-[1200px]">
-      <NuxtImg placeholder src="/logo.svg" alt="Logo" width="32" height="32" />
+      <NuxtLink to="/">
+        <NuxtImg placeholder src="/logo.svg" alt="Logo" width="32" height="32" />
+      </NuxtLink>
       <div class="flex flex-row gap-4">
         <Button class="z-[4000]" @click="scrollToContact" variant="primary" size="small">Get In Touch</Button>
       </div>
