6b6268487db3889efb216fd194ae3f1dc327880c
berkekayada <berkekayada34@gmail.com>
2024-12-27 22:10:21 +0300

    favicongenerator added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
             "Robi Harid is an accomplished entrepreneur with over 10 years' experience leading projects for global brands including TOSHIBA and HSBC.",
         },
         { property: "og:image", content: "/public/robi-harid-main.svg" },
-        { property: "og:url", content: "https://robi-harid.vercel.app" },
+        { property: "og:url", content: "https://robiharid.nuxt.dev" },
         { property: "og:type", content: "website" },
         { name: "twitter:title", content: "Robi Harid" },
         {
@@ -36,7 +36,15 @@ export default defineNuxtConfig({
         },
         { name: "twitter:image", content: "/public/robi-harid-main.svg" },
       ],
-      link: [{ rel: "canonical", href: "https://robi-harid.vercel.app" }],
+      link: [
+        { rel: "canonical", href: "https://robiharid.nuxt.dev" },
+        { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "shortcut icon", href: "/favicon.ico" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        { rel: "manifest", href: "/site.webmanifest" },
+        { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+      ],
     },
   },
 });
